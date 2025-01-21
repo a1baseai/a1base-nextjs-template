c4c39146ce3c63d4af0a6000fff577792bedf999
pasha rayan <pasha.rayan@gmail.com>
2025-01-21 12:04:29 -0800

    removed vercel button

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,12 @@ export default function Home() {
         </p>
 
         <a
-          href={`https://wa.me/${process.env.A1BASE_AGENT_NUMBER?.replace('+', '')}`}
+          href={`https://wa.me/${process.env.A1BASE_AGENT_NUMBER?.replace(
+            "+",
+            ""
+          )}`}
           target="_blank"
-          rel="noopener noreferrer" 
+          rel="noopener noreferrer"
           className="rounded-lg px-6 py-3 bg-[#25D366] hover:bg-[#128C7E] text-white flex items-center gap-2 transition-colors"
         >
           {`Chat with your agent on WhatApp`}
@@ -58,21 +61,6 @@ export default function Home() {
         </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-a1base-app&utm_medium=appdir-template-tw&utm_campaign=create-a1base-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            {`Deploy on Vercel`}
-          </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://docs.a1base.com/"
